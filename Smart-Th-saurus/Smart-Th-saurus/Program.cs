468088b945ceed143168a0e0aec530ca8ecb675f
Imacutekayx <imkayxy@gmail.com>
2018-01-16 15:21:46 +0100

    Création du projet Visual Studio
    Création du projet Visual Studio en C# Console

---
+++ Smart-Th-saurus/Smart-Th-saurus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Th_saurus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = "https://www.etml.ch/";
+            HTMLLinkFinder linkFinder = new HTMLLinkFinder();
+            linkFinder.Finder(URL);
+            Console.ReadLine();
+        }
+    }
+}
