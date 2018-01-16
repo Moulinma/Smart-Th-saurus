34c532b1085e057f2701b6bdf6bd8c6afd59921d
Imacutekayx <imkayxy@gmail.com>
2018-01-16 15:24:09 +0100

    Création d'un chercheur de liens HTML
    Création d'un chercheur de lien HTML qui analyse le string retourner par le TXTCreating et affiche dans le résultat dans la console

---
+++ Smart-Th-saurus/Smart-Th-saurus/HTMLLinkFinder.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Th_saurus
+{
+    class HTMLLinkFinder
+    {
+        public void Finder(string URL)
+        {
+            char chrOne = ' ';
+            char chrTwo = ' ';
+            char chrThree = ' ';
+            char chrFour = ' ';
+            string Link = @"";
+            bool takeCar = false;
+            int chrRemaining = 0;
+            TXTCreating txt = new TXTCreating();
+            string HTML = txt.TakeHTML(URL);
+            for (int x = 0; x < HTML.Length; ++x)
+            {
+                chrOne = chrTwo;
+                chrTwo = chrThree;
+                chrThree = chrFour;
+                chrFour = Convert.ToChar(HTML.Substring(x, 1));
+
+                if(chrThree == '<' && chrFour == 'a')
+                {
+                    takeCar = true;
+                    chrRemaining = 4;
+                }
+                if(chrOne == '<' && chrTwo == '/' && chrThree == 'a' && chrFour == '>')
+                {
+                    takeCar = false;
+                    Console.WriteLine(Link + "\n\n");
+                    Link = "";
+                }
+                if (takeCar)
+                {
+                    if(chrRemaining == 0)
+                    {
+                        Link += chrOne;
+                    }
+                    else
+                    {
+                        --chrRemaining;
+                    }
+                }
+            }
+        }
+    }
+}
