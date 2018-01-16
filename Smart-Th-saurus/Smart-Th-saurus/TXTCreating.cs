7f9489154441c126cd2e20de95112f33131803ef
Imacutekayx <imkayxy@gmail.com>
2018-01-16 15:23:00 +0100

    Création d'un analyseur de page web
    Création d'un analyseur de page web qui retourne l'HTML de la page demandée en string

---
+++ Smart-Th-saurus/Smart-Th-saurus/TXTCreating.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace Smart_Th_saurus
+{
+    class TXTCreating
+    {
+        WebClient client = new WebClient();
+        public TXTCreating()
+        {
+            
+        }
+
+        public string TakeHTML(string URL)
+        {
+            string codeHTML = client.DownloadString(URL);
+            return codeHTML;
+        }
+
+        public void CreateTXT(string URL)
+        {
+            
+        }
+    }
+}
