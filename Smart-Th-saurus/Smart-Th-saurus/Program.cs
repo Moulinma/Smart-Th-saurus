8a0ea2f78effddc45cd868e9833174fc81356a91
Briand Grégory <briandgr@etmlnet.local>
2018-02-13 16:25:08 +0100

    Ajout de la création d'un txt et de l'écriture des mots dedans

--- Smart-Th-saurus/Smart-Th-saurus/Program.cs
+++ Smart-Th-saurus/Smart-Th-saurus/Program.cs
@@ -13,6 +13,7 @@ namespace Smart_Th_saurus
             string URL = "https://www.etml.ch/";
             List<List<string>> TempWordLst = new List<List<string>>();
             List<string> lstLinks = new List<string>();
+            List<string> lstLinksName = new List<string>();
             TXTCreating Creator = new TXTCreating();
             HTMLTxtAnalyser Analyser = new HTMLTxtAnalyser();
 
@@ -25,33 +26,36 @@ namespace Smart_Th_saurus
             foreach(string link in lstLinks)
             {
                 //TODO Gérer les exceptions
-                if(link != "/lecole-2/presentation.html")
+                if(link != "/lecole-2/presentation.html" && link.Substring(0, 1) != "#")
                 {
-                    if(link.Substring(0,1) == "#") { }
-                    else if (link.Substring(0, 1) == "/")
+                    if (link.Substring(0, 1) == "/")
                     {
                         TempWordLst.Add(Analyser.WordSearching(Creator, URL + link.Substring(1, link.Length - 1)));
+                        lstLinksName.Add(link.Substring(1, link.Length - 1));
                     }
                     else if (link == URL)
                     {
                         TempWordLst.Add(Analyser.WordSearching(Creator, link));
+                        lstLinksName.Add("Base");
                     }
                 }
             }
+            
+            //TEST WRITE CONSOLE
+            //foreach(List<string> lstWords in TempWordLst)
+            //{
+            //    foreach(string word in lstWords)
+            //    {
+            //        Console.WriteLine(word);
+            //    }
+            //    Console.WriteLine("--------------------------------------------");
+            //}
 
-            int Test = 0;
-            foreach(List<string> lstWords in TempWordLst)
-            {
-                foreach(string word in lstWords)
-                {
-                    Console.WriteLine(word);
-                }
-                Console.WriteLine("--------------------------------------------");
-                Test++;
-            }
-            Console.Write(Test);
             //Création du Texte
-            //Creator.CreateTXT(TempWordLst);
+            for(int x = 0; x < lstLinksName.Count(); ++x)
+            {
+                Creator.CreateTXT(lstLinksName[x], TempWordLst[x]);
+            }
 
             Console.ReadLine();
         }
--- Smart-Th-saurus/Smart-Th-saurus/TXTCreating.cs
+++ Smart-Th-saurus/Smart-Th-saurus/TXTCreating.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace Smart_Th_saurus
 {
@@ -22,35 +24,20 @@ namespace Smart_Th_saurus
             return codeHTML;
         }
 
-        public void CreateTXT(List<string> lstWords)
+        public void CreateTXT(string Name, List<string> lstWords)
         {
-            string currentString;
-            bool WaitChar = false;
-            foreach(string listWord in lstWords)
+            Name = Regex.Replace(Name, @"/", "_");
+            Name = Regex.Replace(Name, @"\?", "_");
+            Name = Regex.Replace(Name, @"=", "_");
+            string path = Name + ".txt";
+            FileStream stream = File.Create(path);
+            StreamWriter t = new StreamWriter(stream);
+            foreach (string word in lstWords)
             {
-                //TODO Trouver un moyen de créer un txt
-                for (int x = 0; x < listWord.Length - 1; ++x)
-                {
-                    currentString = listWord.Substring(x, 2);
-                    if (currentString == "\n")
-                    {
-                        WaitChar = true;
-                        //TODO mettre les mots à la ligne
-                    }
-                    else if (x == listWord.Length - 2 && !WaitChar)
-                    {
-                        //TODO rajoute le string entier
-                    }
-                    else if (!WaitChar)
-                    {
-                        //TODO rajouter seulement le premier caractère du string
-                    }
-                    else
-                    {
-                        WaitChar = false;
-                    }
-                }
+                t.WriteLine(word);
             }
+            t.Close();
+            stream.Close();
         }
     }
 }
