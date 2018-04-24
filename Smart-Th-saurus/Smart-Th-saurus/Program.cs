741b59fb91590da7899a7ee1d3eb4aee6312ec4c
Briand Grégory <briandgr@etmlnet.local>
2018-04-24 15:37:38 +0200

    Fix de bugs mineurs

--- Smart-Th-saurus/Smart-Th-saurus/Program.cs
+++ Smart-Th-saurus/Smart-Th-saurus/Program.cs
@@ -19,6 +19,8 @@ namespace Smart_Th_saurus
         /// 
         static void Main(string[] args)
         {
+            //TODO [GLO] Tests
+            //TODO [GLO] Opérateurs
             while (true)
             {
                 Console.Write("What do you want to do?\n" +
